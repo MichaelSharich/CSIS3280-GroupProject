3d1046055762eef7251458d077e27cbba82d9808
Kade Salvo <61894063+KadeSalvo@users.noreply.github.com>
2021-04-17 13:57:45 -0600

    Add files via upload

--- GroupProjectWPF/Main/clsMainSQL.cs
+++ GroupProjectWPF/Main/clsMainSQL.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +13,35 @@ namespace GroupProjectWPF.Main
 {
     class clsMainSQL
     {
+
+        clsDataAccess db = new clsDataAccess();
+
+        /// <summary>
+        /// This SQL is used to add a new Invoice to the database
+        /// Will use: INSERT INTO Invoices
+        /// </summary>
+        public void CreateInvoice()
+        {
+
+        }
+
+        /// <summary>
+        /// This SQL is used to Edit an invoice's information 
+        /// Will use: UPDATE Invoices SET [part of invoice] WHERE [related] = [user submitted data]
+        /// </summary>
+        public void EditInvoice()
+        {
+
+        }
+
+        /// <summary>
+        /// This SQL is used to delete an invoice from the database
+        /// Will use: DELETE From Invoices WHERE InvoiceNum = [user submitted int]
+        /// </summary>
+        public void DeleteInvoice(int inNum)
+        {
+
+        }
+
     }
 }
--- GroupProjectWPF/Main/wndMain.xaml.cs
+++ GroupProjectWPF/Main/wndMain.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using GroupProjectWPF.Search;
+using GroupProjectWPF.Items;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Reflection;
 
 namespace GroupProjectWPF.Main
 {
@@ -20,17 +22,172 @@ namespace GroupProjectWPF.Main
     /// </summary>
     public partial class wndMain : Window
     {
+        #region attributes
+        /// <summary>
+        /// Connection to the database
+        /// </summary>
+        clsDataAccess db;
+
+        /// <summary>
+        /// Connection to the Mains logic
+        /// </summary>
+        clsMainLogic ml;
+
+        /// <summary>
+        /// Holds the interaction with the database
+        /// </summary>
+        clsMainSQL sMS;
+
+        /// <summary>
+        /// Class that holds the search functionality
+        /// </summary>
+        wndSearch searchWin;
+
+        /// <summary>
+        /// Class that holds the update functionality. Not working
+        /// </summary>
+        //wndItems itemsWin;
+
+        //Grants access to the list of invoices
+        List<Invoice> invoices;
+
+        #endregion
+        /// <summary>
+        /// Initializes the window
+        /// </summary>
         public wndMain()
         {
             InitializeComponent();
+
+            db = new clsDataAccess();
+            ml = new clsMainLogic();
+            sMS = new clsMainSQL();
+
+            searchWin = new wndSearch();
+            //itemsWin = new wndItems();
+            invoices = new List<Invoice>();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Sends the user to the search window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            wndSearch SearchWnd = new wndSearch();
-            SearchWnd.Activate();
-            SearchWnd.Show();
-            this.Hide();
+            try
+            {
+                //wndSearch SearchWnd = new wndSearch();
+                searchWin.Activate();
+                this.Hide();
+                searchWin.ShowDialog();
+                this.Show();
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Sends the user to the update items window. Can't get this to work for some reason
+        /// </summary>
+        /// <param name="sender"></param>
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // wndItems winItem = new wndItems();
+                // winItem.Activate();
+                //this.Hide();
+                // winItem.ShowDialog();
+                // this.Show();
+                // winItem.Close();
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Handle the error.
+        /// </summary>
+        /// <param name="sClass">The class in which the error occurred in.</param>
+        /// <param name="sMethod">The method in which the error occurred in.</param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                //Would write to a file or database here.
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine +
+                                             "HandleError Exception: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Accesses the create functionality to make a new invoice
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Accesses the Edit functionality to edit an existing invoice.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Accesses the Delete functionality to delete an invoice
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
     }
 }
